3090762a9761e312ed4f2a7b1ac91f48aa2d52f4
Aria1ink <ann.arialink@yandex.ru>
2023-10-04 13:54:20 +0300

    save

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
 
+import catchErrors from "middlewares/catchErrors";
+
 const app = express();
 mongoose.set("strictQuery", false);
 mongoose.connect("mongodb://localhost:27017/mestodb");
+
+app.use(catchErrors);
+
 app.listen(3000);
---
+++ src/middlewares/catchErrors.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from "express";
+
+interface ErrorWithStatus extends Error {
+  statusCode: number;
+}
+
+export default function catchErrors(err: ErrorWithStatus, req: Request, res: Response, next: NextFunction) {
+  const { statusCode = 500, message } = err;
+
+  res
+    .status(statusCode)
+    .send({
+      message: statusCode === 500
+        ? 'Internal server error'
+        : message
+      });
+  };
